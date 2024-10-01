2f8f5371ac0a029e68cd7aa9fe01a173aac73974
Z1mniy <zimniy1224@gmail.com>
2024-10-01 19:22:24 +0300

    add description for function in code

--- circle.py
+++ circle.py
@@ -1,10 +1,10 @@
 import math
 
-
+#in circle radius (r), out circle area
 def area(r):
     return math.pi * r * r
 
-
+#in circle radius (r), out circle perimeter
 def perimeter(r):
     return 2 * math.pi * r
 
